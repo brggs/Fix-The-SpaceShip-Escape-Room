60ecc105b1c2142ee5b9334fbdb55c64a2c04da8
Andrew B <andrew.b@bluefinch.uk>
2024-06-08 17:41:10 +0100

    Simplified step 1

--- src/step-1/robots.py
+++ src/step-1/robots.py
@@ -1,7 +1,6 @@
 from microbit import *
-import radio
 
-POLL_INTERVAL = 300
+POLL_INTERVAL = 50
 
 ROBOT_ICONS = {
     1: Image.STICKFIGURE,
@@ -25,23 +24,25 @@ while choosing_role:
 
 # The robot is now ready for the game.  
 # Wait for the correct input send a message.
-
-display.show(ROBOT_ICONS[role])
-
-radio.config(group=1, power = 3)
-radio.on()
+# This input is different for each robot.
 
 while True:
+    display.show(ROBOT_ICONS[role])
 
     if role == 1 and button_a.was_pressed():
-        radio.send("0")
-    elif role == 2 and button_a.was_pressed():
-        radio.send("0")
-    elif role == 3 and button_a.was_pressed():
-        radio.send("0")
-    elif role == 4 and button_a.was_pressed():
-        radio.send("0")
-    elif role == 5 and button_a.was_pressed():
-        radio.send("1")
+        display.show(4)
+        sleep(1000)
+    elif role == 2 and button_a.is_pressed() and button_b.is_pressed():
+        display.show(5)
+        sleep(1000)
+    elif role == 3 and accelerometer.was_gesture('down'):
+        display.show(2)
+        sleep(1000)
+    elif role == 4 and accelerometer.was_gesture('shake'):
+        display.show(8)
+        sleep(1000)
+    elif role == 5 and display.read_light_level() < 20:
+        display.show(7)
+        sleep(1000)
     
     sleep(POLL_INTERVAL)
